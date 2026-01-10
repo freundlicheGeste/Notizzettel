6d7d72336dd979af8df92b81549c3e875775e8cf
freundlicheGeste <freundlichegeste@gmail.com>
2026-01-10 22:17:04 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'app-cache-v1';
+const urlsToCache = [
+  'notizen.html',
+  'punkterechner.html',
+  'manifest.json'
+];
+
+// Dateien installieren und im Cache speichern
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Bei Netz-Anfragen zuerst im Cache suchen
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
